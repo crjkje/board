a4f3a4bdedf1a8f0eb2c6fe3f291088cd18918a8
Jung-Kwon Lee <akdirdl@gmail.com>
2022-11-30 23:30:02 +0900

    feat: 양방향 암호화 클래스 생성

---
+++ src/main/java/com/example/board/biz/domain/auth/CryptUtils.java
@@ -0,0 +1,26 @@
+package com.example.board.biz.domain.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+
+@Service
+@RequiredArgsConstructor
+public class CryptUtils {
+    private final AesBytesEncryptor aesBytesEncryptor;
+    String a = "test";
+    public String encryptEmail(String email) {
+        byte[] encrypt = aesBytesEncryptor.encrypt(email.getBytes(StandardCharsets.UTF_8));
+
+        return new String(encrypt, StandardCharsets.UTF_8);
+    }
+
+    public String decryptEmail(String encryptedEmail) {
+        byte[] decryptEmail = encryptedEmail.getBytes();
+        byte[] decrypt = aesBytesEncryptor.decrypt(decryptEmail);
+        return new String(decrypt, StandardCharsets.UTF_8);
+    }
+
+}
