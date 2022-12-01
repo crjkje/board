11793f84e6b5cf51270fc7f5b0ea243697fe3d7a
Jung-Kwon Lee <akdirdl@gmail.com>
2022-12-01 22:54:20 +0900

    feat: 회원가입 기능을 위한 UserDetailsService 생성

---
+++ src/main/java/com/example/board/biz/domain/auth/PrincipalDetailsService.java
@@ -0,0 +1,38 @@
+package com.example.board.biz.domain.auth;
+
+import com.example.board.biz.domain.member.Member;
+import com.example.board.biz.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException(username + "을 찾지 못하였습니다."));
+    }
+
+    private UserDetails createUserDetails(Member member) {
+        SimpleGrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthority().toString());
+        return new User(
+                member.getEmail(),
+                member.getPassword(),
+                Collections.singleton(grantedAuthority)
+        );
+    }
+}
