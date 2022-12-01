d87fa947db2d2aa18a1f782f47c0907ce8bcea48
Jung-Kwon Lee <akdirdl@gmail.com>
2022-12-01 22:41:37 +0900

    feat: AesBytesEncryptor 빈 등록

--- src/main/java/com/example/board/biz/config/SecurityConfig.java
+++ src/main/java/com/example/board/biz/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.example.board.biz.config;
 
 
+import com.example.board.biz.domain.auth.CryptUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,9 +17,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${aesByteEncryptor.secret}")
+    String secret;
+
+    @Value("${aesByteEncryptor.salt}")
+    String salt;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AesBytesEncryptor aesBytesEncryptor() {
+        return new AesBytesEncryptor(secret, salt);
     }
 
     @Bean
